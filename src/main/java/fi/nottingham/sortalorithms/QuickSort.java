7a35cc34df1f46899dc49fc127c6746e148e4a6f
Wiltzu <villeahti@hotmail.com>
2013-03-10 11:05:11 +0200

    QuickSort started

---
+++ src/main/java/fi/nottingham/sortalorithms/QuickSort.java
@@ -0,0 +1,11 @@
+package fi.nottingham.sortalorithms;
+
+public class QuickSort implements SortAlgorithm {
+
+	@Override
+	public <T extends Comparable<? super T>> void sort(T[] array) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
