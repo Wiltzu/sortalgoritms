7a3631420172c80eeede03ffa43000d1eb935f5d
Wille2 <villeahti@hotmail.com>
2013-03-10 22:20:24 +0200

    Counting sort started + abstractSortalgorithm refactored

--- src/main/java/fi/nottingham/sortalorithms/AbstractSortAlgorithm.java
+++ src/main/java/fi/nottingham/sortalorithms/AbstractSortAlgorithm.java
@@ -2,13 +2,11 @@ package fi.nottingham.sortalorithms;
 
 public abstract class AbstractSortAlgorithm implements SortAlgorithm {
 
-
-
 	protected <T extends Comparable<? super T>> void swap(int first,
 			int second, T[] array) {
-				T tmp = array[first];
-				array[first] = array[second];
-				array[second] = tmp;
-			}
+		T tmp = array[first];
+		array[first] = array[second];
+		array[second] = tmp;
+	}
 
 }
---
+++ src/main/java/fi/nottingham/sortalorithms/CountingSort.java
@@ -0,0 +1,12 @@
+package fi.nottingham.sortalorithms;
+
+public class CountingSort {
+
+	public static int[] sort(int[] array, int low, int high) {
+		
+		int[] count = new int[high - low + 1];
+		
+		
+		return null;
+	}
+}
